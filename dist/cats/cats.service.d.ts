0d897b713a85ce7db7a64615bccec8c10f81cc56
wangjidong1 <wangjidong1@kingsoft.com>
2019-10-21 17:05:14 +0800

    clear

--- dist/cats/cats.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Cat } from './interfaces/cats.interface';
-export declare class CatsService {
-    private readonly cats;
-    create(cat: Cat): void;
-    findAll(): Cat[];
-    hello(): string;
-}
